5f102a823dfc38983def36666e0ab7ceeb1baf0a
Sergey Kosykh <s3rkosal@gmail.com>
2026-03-10 18:39:24 +0400

    basic proseMirror setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,39 @@
 import './style.css'
 
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import { Schema, DOMParser } from "prosemirror-model";
+import { schema } from "prosemirror-schema-basic";
+import { addListNodes } from "prosemirror-schema-list"
+import { exampleSetup } from "prosemirror-example-setup";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+import type { EditorStateConfig } from 'prosemirror-state';
 
-    <h1>Init project</h1>
+// Mix the nodes from prosemirror-schema-list into the basic schema to
+// create a schema with list support.
+const mySchema = new Schema({
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+  marks: schema.spec.marks
+});
 
-  </div>
-`
+
+// editor config
+let editorStateConfig: EditorStateConfig = {
+  plugins: exampleSetup({ schema: mySchema })
+};
+
+const initialContent = document.querySelector("#content");
+if (initialContent) {
+  editorStateConfig.doc = DOMParser.fromSchema(mySchema).parse(initialContent);
+}
+
+// editor state
+let editorState = EditorState.create(editorStateConfig);
+
+
+
+// editor view
+new EditorView(document.querySelector<HTMLDivElement>('#editor'), {
+  state: editorState
+});
+
